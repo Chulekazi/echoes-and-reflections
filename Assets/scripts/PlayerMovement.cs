f5150bd7074b5ee88b0cf23ae770756c0816e26e
Chulekazi <2623892@students.wits.ac.za>
2026-07-11 03:18:52 +0200

    fixed player movement

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+
     private Rigidbody2D rb;
-    private Vector2 movementInput;
-    public Animator anim;
-    public SpriteRenderer sprite;
+    private Vector2 moveInput;
 
     void Start()
     {
@@ -15,41 +14,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movementInput = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            movementInput.y = 1;  
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            movementInput.y = -1; 
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            movementInput.x = 1; 
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            movementInput.x = -1;
-        }
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
     }
 
     void FixedUpdate()
     {
-        Vector2 isoMovement = ConvertToIsometric(movementInput);
-        rb.linearVelocity = isoMovement.normalized * moveSpeed;
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
-
-    Vector2 ConvertToIsometric(Vector2 standardInput)
-    {
-        float isoX = standardInput.x - standardInput.y;
-        float isoY = (standardInput.x + standardInput.y) * 0.5f;
-
-        return new Vector2(isoX, isoY);
-    }
-    
 }
-
-
